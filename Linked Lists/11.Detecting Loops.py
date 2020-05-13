feb6075b16261c9a4ca3145252c13b632825d5e7
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-13 23:32:57 +0530

    detecting loops

---
+++ Linked Lists/11.Detecting Loops.py
@@ -0,0 +1,40 @@
+#%%
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head  = None
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+    def push(self, new_data): 
+        new_node = Node(new_data) 
+        new_node.next = self.head 
+        self.head = new_node 
+    def detectLoop(self):
+        s=set()
+        temp=self.head
+        while(temp):
+            if temp in s:
+                return True
+            s.add(temp)
+            temp=temp.next
+        return False
+llist = LinkedList() 
+llist.push(20) 
+llist.push(4) 
+llist.push(15) 
+llist.push(10) 
+llist.head.next.next.next.next = llist.head
+if( llist.detectLoop()): 
+    print ("Loop found") 
+else : 
+    print ("No Loop ") 
+
+
+# %%
