7b0b1309153ca046143649318260a55c8780be40
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-09-07 23:05:33 +0530

    inorder with stack

---
+++ Trees/BinaryTrees/4.InorderWithStack.py
@@ -0,0 +1,31 @@
+#%%
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def inOrder(root):
+    current = root
+    stack = []
+    complete = 0
+    while True:
+        if current is not None:
+            stack.append(current)
+            current = current.left
+        elif(stack):
+            current = stack.pop()
+            print(current.data, end=" ")
+            current = current.right 
+        else: 
+            break
+        
+root = Node(1) 
+root.left = Node(2) 
+root.right = Node(3) 
+root.left.left = Node(4) 
+root.left.right = Node(5) 
+  
+inOrder(root) 
+  
+# %%
