0e3c24b961d1a63919742e4741238f6a95cbc41b
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-09-07 22:47:24 +0530

    traversals

---
+++ Trees/BinaryTrees/3.Traversals.py
@@ -0,0 +1,38 @@
+#%%
+class Node():
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.value = key
+
+def printTreeInorder(root):
+    if root:
+        printTreeInorder(root.left)
+        print(root.value)
+        printTreeInorder(root.right)
+
+def printTreePreorder(root):
+    if root:
+        print(root.value)
+        printTreeInorder(root.left)
+        printTreeInorder(root.right)  
+        
+def printTreePostorder(root):
+    if root:
+        printTreeInorder(root.left)
+        printTreeInorder(root.right)  
+        print(root.value)
+
+root = Node(1) 
+root.left = Node(2) 
+root.right = Node(3) 
+root.left.left = Node(4) 
+root.left.right = Node(5) 
+
+print("Inorder")
+printTreeInorder(root)
+print("Preorder")
+printTreePreorder(root)
+print("Postorder")
+printTreePostorder(root)
+# %%
