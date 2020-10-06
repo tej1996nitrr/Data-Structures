1663a9405208ff69f367d8a0383b730d6bb5791a
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-10-06 22:23:39 +0530

    using stack/queue

--- Trees/BinaryTrees/7.ReverseLevelOrder.py
+++ Trees/BinaryTrees/7.ReverseLevelOrder.py
@@ -42,3 +42,41 @@ reverseLevelOrder(root)
 
 
 # %%
+'''Using Stack, Queue'''
+from collections import deque
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def reverseOrder(root):
+    q = deque()
+    q.append(root)
+    ans = deque()
+    
+    while q:
+        node = q.popleft()
+        if node is None:
+            continue
+        ans.appendleft(node.data)
+
+        if node.right:
+            q.append(node.right)
+             
+        if node.left:
+            q.append(node.left)
+    return ans
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+reverseOrder(root)
+
+    
+
+# %%
